1168ed1abef226bf3dccd10ac1199e9afd9072d7
yyi <yyi@fortinet.com>
2020-12-10 20:31:34 -0800

    run multi

--- LV/simple_monitor.py
+++ LV/simple_monitor.py
@@ -2,6 +2,7 @@ from requests_html import HTMLSession
 import sys
 from notifications.messenger import Messenger
 from notifications.mails import Email
+from time import sleep
 
 MESSENGER_LIST={'Katherine Nguyen': '100003870432163',
                 'Jolin Lu': '100009501767563'}
@@ -24,23 +25,30 @@ if len(sys.argv) < 2:
 pwd=sys.argv[1]
 email = "yaqixyzlancelot@gmail.com"
 
-for url in PRODUCT_URLS:
-    identifier = url.split('-')[-1]
-    api_endpoint = API_BASE % identifier
-    response = session.get(api_endpoint)
-    data = response.json()
-    print(data)
-    for availability in data['skuAvailability']:
-        if availability['inStock'] == True:
-            print('restocked')
-            if gmail is None:
-                gmail=Email(email, pwd)
-            msg = f'THE FOLLOWING PRODUCT HAS RESTOCKED -> {url}'
-            subject = "LV RESTOCK ALERT"
-            print(msg)
-            for name, email_addr in GMAIL_LIST.items():
-                gmail.send_msg(email_addr, subject, msg)
-            break
-        else:
-            print('out of stock')
+def check():
+    for url in PRODUCT_URLS:
+        identifier = url.split('-')[-1]
+        api_endpoint = API_BASE % identifier
+        response = session.get(api_endpoint)
+        data = response.json()
+        print(data)
+        for availability in data['skuAvailability']:
+            if availability['inStock'] == True:
+                print('restocked')
+                if gmail is None:
+                    gmail=Email(email, pwd)
+                msg = f'THE FOLLOWING PRODUCT HAS RESTOCKED -> {url}'
+                subject = "LV RESTOCK ALERT"
+                print(msg)
+                for name, email_addr in GMAIL_LIST.items():
+                    gmail.send_msg(email_addr, subject, msg)
+                break
+            else:
+                print('out of stock')
+
+if __name__=='__main':
+    for i in range(8):
+        check()
+        sleep(60)
+
 
