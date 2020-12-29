59be095e65524f10f8f55b26b6221dc29e035da3
yyi <yyi@fortinet.com>
2020-12-28 19:46:35 -0800

    added file_ops

--- util/files_ops.py
+++ util/files_ops.py
@@ -1,6 +1,19 @@
-
+import glob, os, sys
+import datetime as dt
 
 def remove_old_files(folder, day):
     '''remove files older than
        'day' days from folder
     '''
+    for file in glob.glob(folder):
+        last_changed = os.path.getmtime(file)
+        last_changed = dt.datetime.fromtimestamp(last_changed)
+        days_diff = (dt.datetime.now() -  last_changed).days
+        if days_diff > day:
+            print(f'removing {file}')
+            os.remove(file)
+
+if __name__ == '__main__':
+    folder = sys.argv[1]
+    day = int(sys.argv[2])
+    remove_old_files(folder, day)
